e722b4dcfff5b1e669594cb416e6aaecf321ef81
Trang <pctrangmeo161@gmail.com>
2024-04-10 05:41:44 +0900

    added image hosts allowed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{ hostname: "lh3.googleusercontent.com"}]
-    },
+  images: {
+    remotePatterns: [
+      {
+        hostname: [
+          "lh3.googleusercontent.com",
+          "lh3.google.com",
+          "static.mercdn.net",
+        ],
+      },
+    ],
+  },
 };
 
 export default nextConfig;
