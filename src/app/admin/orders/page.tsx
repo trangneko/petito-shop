81be83ebec7fd99e9d8bd320ea47095a27139f4f
Trang <pctrangmeo161@gmail.com>
2024-04-10 05:25:26 +0900

    minor fixes

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -35,7 +35,6 @@ export default function OrdersPage() {
       <div className="flex justify-between items-center gap-4">
         <PageHeader>Đơn hàng</PageHeader>
       </div>
-      {/* @ts-expect-error Async Server Component */}
       <OrdersTable />
     </>
   );
