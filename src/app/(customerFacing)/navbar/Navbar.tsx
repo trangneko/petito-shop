c2e54ec03a2c7e27f6e8407f08143fbbd763275a
Bui Duc Anh <s1f102010018@iniad.org>
2024-04-20 18:14:13 +0900

    added style in navbar, footer

--- src/app/(customerFacing)/navbar/Navbar.tsx
+++ src/app/(customerFacing)/navbar/Navbar.tsx
@@ -6,9 +6,9 @@ import { Input } from "@/components/ui/input";
 import { getCart } from "@/db/cart";
 import UserMenuButton from "@/components/UserMenuButton";
 import { auth } from "@/auth";
+import { SearchIcon } from "lucide-react";
 // import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 // import  getServerSession from "next-auth";
-
 
 async function searchProducts(formData: FormData) {
   "use server";
@@ -26,19 +26,29 @@ export default async function Navbar() {
 
   return (
     <Nav>
-      <NavLink href="/">Trang chủ</NavLink>
-      <NavLink href="/products">Sản phẩm</NavLink>
-      <NavLink href="/orders">Đơn đã đặt</NavLink>
+      <div>Logo</div>
 
-      <form action={searchProducts} className="flex items-center">
-        <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input type="text" name="searchQuery" placeholder="Tìm sản phẩm..." />
-          <Button type="submit">Tìm kiếm</Button>
-        </div>
-      </form>
+      <div>
+        <NavLink href="/">Trang chủ</NavLink>
+        <NavLink href="/products">Sản phẩm</NavLink>
+        <NavLink href="/orders">Đơn đã đặt</NavLink>
+      </div>
 
-      <ShoppingCartButton cart={cart} />
-      <UserMenuButton session={session} />
+      <div className="flex gap-4">
+        <form action={searchProducts} className="flex items-center">
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input
+              type="text"
+              name="searchQuery"
+              placeholder="Tìm sản phẩm..."
+            />
+            <Button type="submit"><SearchIcon/></Button>
+          </div>
+        </form>
+
+        <ShoppingCartButton cart={cart} />
+        <UserMenuButton session={session} />
+      </div>
     </Nav>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
+import { Facebook, Instagram } from "lucide-react";
 import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="flex justify-between py-8">
+      <div className="flex justify-between py-8 px-8 max-w-7xl al m-auto">
         <div className="w-40 h-40 relative">
           <Image src="/ava.png" alt="Petito thumbnail" fill priority={true}
           sizes="(max-width: 768px) 100px, (max-width: 1200px) 200px, 200px"/>
         </div>
         <div className="">
-          <h5>Follow Us</h5>
-          <ul>
-            <li>Facebook</li>
-            <li>Instargram</li>
+          <h5 className="text-right font-bold">Follow Us</h5>
+          <ul className="flex gap-3 my-4">
+            <li><Facebook size={48} color="#001299" /></li>
+            <li><Instagram size={48} color="#ff004c" /></li>
           </ul>
         </div>
       </div>
